cdecbe06cf3510667b5d4906f23d5a6102edcb34
Christian Schlotter <christi.schlotter@gmail.com>
2026-05-31 16:06:14 +0200

    some old refactoring

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/chrischdi/promshift-proxy
 
 go 1.14
+
+require (
+	github.com/json-iterator/go v1.1.12
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/common v0.32.1
+)
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -10,6 +11,10 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	json "github.com/json-iterator/go"
+	"github.com/pkg/errors"
+	"github.com/prometheus/common/model"
 )
 
 var (
@@ -17,12 +22,11 @@ var (
 	listen = flag.String("listen", "0.0.0.0:9090", "listen addr")
 	debug  = flag.Bool("debug", false, "log level")
 
-	targetURL           *url.URL
-	timeshiftHeadersMap = map[string]bool{
-		"promshift_start": true,
-		"promshift_end":   true,
-		"promshift_time":  true,
-	}
+	targetURL *url.URL
+)
+
+const (
+	pathReduceMonthOverlap = "/reduce-month-overlap"
 )
 
 func main() {
@@ -36,99 +40,194 @@ func main() {
 	}
 
 	log.Printf("Listening on %s", *listen)
-	http.HandleFunc("/", serveRequest)
+	http.HandleFunc(pathReduceMonthOverlap+"/", removeMonthOverlap)
+	// http.HandleFunc("/", forward)
 	if err := http.ListenAndServe(*listen, nil); err != nil {
 		log.Fatalf("Error handling: %v", err)
 	}
 }
 
-func serveRequest(w http.ResponseWriter, req *http.Request) {
+func forward(w http.ResponseWriter, req *http.Request) {
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	log.Printf("[fwr] Request: %s %s %s %s %s", req.Method, req.Host, req.URL.String(), req.RequestURI, req.URL.RawQuery)
+	proxy.ServeHTTP(w, req)
+}
 
-	req.URL.Host = targetURL.Host
-	req.URL.Scheme = targetURL.Scheme
-	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
-	req.Host = targetURL.Host
-
+func removeMonthOverlap(w http.ResponseWriter, req *http.Request) {
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	log.Printf("[rmo] Request: %s %s %s %s %s", req.Method, req.Host, req.URL.String(), req.RequestURI, req.URL.RawQuery)
 	if *debug {
-		log.Printf("RawQuery before change: %s", req.URL.RawQuery)
+		out, _ := httputil.DumpRequest(req, true)
+		log.Printf("Request: %s", out)
 	}
-	req.URL.RawQuery = shiftRawQuery(req.URL.RawQuery)
-	if *debug {
-		log.Printf("RawQuery after change : %s", req.URL.RawQuery)
+	// rewrite the director to trim path prefix on forwarding the request
+	originalDirector := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		requestTrimPathPrefix(req, pathReduceMonthOverlap)
+		// enforce no encoding so we are able to modify the response properly
+		req.Header.Del("Accept-Encoding")
+	}
+	// add ModifyResponse func
+	proxy.ModifyResponse = func(r *http.Response) error {
+		err := modifyResponse(r)
+		if err != nil {
+			log.Printf("error on modifyFunc: %v", err)
+		}
+		return err
 	}
 	proxy.ServeHTTP(w, req)
 }
 
-// shiftRawQuery shifts the RawQuery based on url parameters.
-// url.Values is not used because it is not compatible to the query parameter.
-func shiftRawQuery(query string) string {
-	changes := map[string]int64{}
-	resultValues := []string{}
-
-	// split headers to slice, filter for `promshift_` headers and parse them
-	kvs := []string{}
-	for _, kv := range strings.Split(query, "&") {
-		if strings.HasPrefix(kv, "promshift_") {
-			targetHeader, change := parsePromshiftHeader(kv)
-			if targetHeader == "" {
-				// error parsing so let's ignore this one
-				continue
-			}
-			changes[targetHeader] = change
-			// we do not want to keep the header
-			continue
-		}
-
-		// add header to headers to keep it
-		kvs = append(kvs, kv)
-	}
-
-	// apply changes
-	for _, kv := range kvs {
-		s := strings.Split(kv, "=")
-		if len(s) != 2 {
-			resultValues = append(resultValues, kv)
-			continue
-		}
-
-		k := s[0]
-		v := s[1]
-
-		if c, ok := changes[k]; ok {
-			i, err := strconv.ParseInt(strings.Split(v, ".")[0], 10, 64)
-			if err != nil {
-				log.Printf("ERROR: error parsing header %q, value %q: %v", k, v, err)
-				continue
-			}
-			i = i + c
-			v = fmt.Sprintf("%d", i)
-		}
-
-		resultValues = append(resultValues, k+"="+v)
-	}
-
-	return strings.Join(resultValues, "&")
+func requestTrimPathPrefix(req *http.Request, prefix string) {
+	req.RequestURI = strings.TrimPrefix(req.RequestURI, prefix)
+	req.URL.Path = strings.TrimPrefix(req.URL.Path, prefix)
 }
 
-// parsePromshiftHeader parses a promshift header which should be a duration
-// and returns the header to be modified and the value to add.
-func parsePromshiftHeader(h string) (string, int64) {
-	splitted := strings.Split(h, "=")
-	if !timeshiftHeadersMap[splitted[0]] {
-		return "", 0
+func modifyResponse(r *http.Response) error {
+	if *debug {
+		out, _ := httputil.DumpResponse(r, true)
+		log.Printf("Incoming Response: %s", out)
 	}
 
-	promshiftHeader := splitted[0]
-	targetHeader := strings.TrimPrefix(promshiftHeader, "promshift_")
-	v := splitted[1]
-
-	dur, err := time.ParseDuration(v)
+	oldBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Printf("WARN: unable to parse duration %q from header %q", v, promshiftHeader)
-		return "", 0
+		return errors.Wrap(err, "read old body")
 	}
 
-	change := int64(dur.Seconds())
-	return targetHeader, change
+	apiResponse := APIResponse{}
+	if err := json.Unmarshal(oldBody, &apiResponse); err != nil {
+		r.Body = ioutil.NopCloser(bytes.NewReader(oldBody))
+		r.ContentLength = int64(len(oldBody))
+		r.Header.Set("Content-Length", strconv.Itoa(len(oldBody)))
+		log.Printf("ignoring unmarshal APIResponse error: %v", err)
+		return nil
+	}
+
+	if apiResponse.Status != "success" {
+		r.Body = ioutil.NopCloser(bytes.NewReader(oldBody))
+		r.ContentLength = int64(len(oldBody))
+		r.Header.Set("Content-Length", strconv.Itoa(len(oldBody)))
+		return nil
+	}
+
+	if apiResponse.Data == nil {
+		r.Body = ioutil.NopCloser(bytes.NewReader(oldBody))
+		r.ContentLength = int64(len(oldBody))
+		r.Header.Set("Content-Length", strconv.Itoa(len(oldBody)))
+		return nil
+	}
+
+	v := struct {
+		Type   model.ValueType `json:"resultType"`
+		Result json.RawMessage `json:"result"`
+	}{}
+	if err := json.Unmarshal(apiResponse.Data, &v); err != nil {
+		return errors.Wrap(err, "unmarshal APIResponse.Data")
+	}
+
+	if v.Type != model.ValMatrix {
+		r.Body = ioutil.NopCloser(bytes.NewReader(oldBody))
+		r.ContentLength = int64(len(oldBody))
+		r.Header.Set("Content-Length", strconv.Itoa(len(oldBody)))
+		log.Printf("not modifying response, got %s, expected %s", v.Type, model.ValMatrix)
+		return nil
+	}
+	var mv model.Matrix
+	if err := json.Unmarshal(v.Result, &mv); err != nil {
+		return errors.Wrap(err, "unmarshal matrix")
+	}
+
+	var newMv model.Matrix
+	for _, sampleStream := range mv {
+		sampleStream.Values = timeshift(sampleStream.Values)
+		sampleStream.Values = trimFirst(sampleStream.Values)
+		sampleStream.Values = trimLast(sampleStream.Values)
+		newMv = append(newMv, sampleStream)
+	}
+
+	v.Result, err = json.Marshal(newMv)
+	if err != nil {
+		return err
+	}
+
+	apiResponse.Data, err = json.Marshal(&v)
+	if err != nil {
+		return err
+	}
+
+	newBody, err := json.Marshal(&apiResponse)
+	if err != nil {
+		return err
+	}
+
+	// update body and set content length information
+	r.Body = ioutil.NopCloser(bytes.NewReader(newBody))
+	r.ContentLength = int64(len(newBody))
+	r.Header.Set("Content-Length", strconv.Itoa(len(newBody)))
+
+	if *debug {
+		out, _ := httputil.DumpResponse(r, true)
+		log.Printf("Modified Response: %s", out)
+	}
+
+	return nil
 }
+
+func timeshift(pairs []model.SamplePair) []model.SamplePair {
+	new := []model.SamplePair{}
+	for _, p := range pairs {
+		if p.Timestamp.Time().Hour() < 4 {
+			hours := time.Hour * time.Duration(p.Timestamp.Time().Hour())
+			minutes := time.Minute * time.Duration(p.Timestamp.Time().Minute())
+			seconds := time.Second * time.Duration(p.Timestamp.Time().Second())
+			delta := hours + minutes + seconds + time.Second
+			p.Timestamp = p.Timestamp.Add(-delta)
+		}
+		if p.Timestamp.Time().Hour() > 21 {
+			hours := 23*time.Hour - time.Hour*time.Duration(p.Timestamp.Time().Hour())
+			minutes := 59*time.Minute - time.Minute*time.Duration(p.Timestamp.Time().Minute())
+			seconds := 59*time.Second - time.Second*time.Duration(p.Timestamp.Time().Second())
+			delta := hours + minutes + seconds
+			p.Timestamp = p.Timestamp.Add(delta)
+		}
+		new = append(new, p)
+	}
+
+	return new
+}
+
+func trimFirst(pairs []model.SamplePair) []model.SamplePair {
+	if len(pairs) == 0 {
+		return pairs
+	}
+
+	for len(pairs) > 0 && pairs[0].Timestamp.Time().Day() > 15 {
+		pairs = pairs[1:]
+	}
+
+	return pairs
+}
+
+func trimLast(pairs []model.SamplePair) []model.SamplePair {
+	if len(pairs) == 0 {
+		return pairs
+	}
+
+	for len(pairs) > 16 && pairs[len(pairs)-1].Timestamp.Time().Day() <= 16 {
+		pairs = pairs[:len(pairs)-1]
+	}
+
+	return pairs
+}
+
+type (
+	ErrorType   string
+	APIResponse struct {
+		Status    string          `json:"status"`
+		Data      json.RawMessage `json:"data"`
+		ErrorType ErrorType       `json:"errorType"`
+		Error     string          `json:"error"`
+		Warnings  []string        `json:"warnings,omitempty"`
+	}
+)
